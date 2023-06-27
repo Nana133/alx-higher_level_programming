4b94affd26cf3f0e683d52bd9b0771f5a652f723
Nana133 <odosaint@gmail.com>
2023-06-27 09:07:26 -0700

    0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,6 @@
-   #!/usr/bin/python3
-   """ This defines an empty class square """
+#!/usr/bin/python3
+""" This defines an empty class square """
 
-   class square:
+class square:
        """ empty class for task 0 """
        pass
